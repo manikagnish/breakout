91cfb985ef58fb293385c72e81f12c336ff649da
manikagnish <72827130+manikagnish@users.noreply.github.com>
2021-08-10 18:50:04 +0530

    created the bricks

--- index.js
+++ index.js
@@ -3,7 +3,11 @@ const closeBtn = document.getElementById("close-btn");
 const rules = document.getElementById("rules");
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
+
 let score = 0;
+
+const brickRowCount = 9;
+const brickColumnCount = 5;
 
 // Create ball props
 const ball = {
@@ -25,6 +29,27 @@ const paddle = {
   dx: 0,
 };
 
+// Create brick props
+const brickInfo = {
+  w: 70,
+  h: 20,
+  padding: 10,
+  offsetX: 45,
+  offsetY: 60,
+  visible: true,
+};
+
+// create bricks
+const bricks = [];
+for (let i = 0; i < brickRowCount; i++) {
+  bricks[i] = [];
+  for (let j = 0; j < brickColumnCount; j++) {
+    const x = i * (brickInfo.w + brickInfo.padding) + brickInfo.offsetX;
+    const y = j * (brickInfo.h + brickInfo.padding) + brickInfo.offsetY;
+    bricks[i][j] = { x, y, ...brickInfo };
+  }
+}
+console.log(bricks);
 // Draw ball on canvas
 function drawBall() {
   ctx.beginPath();
@@ -43,17 +68,31 @@ function drawPaddle() {
   ctx.closePath();
 }
 
+// draw score on canvas
+function drawScore() {
+  ctx.font = "20px Arial";
+  ctx.fillText(`Score: ${score}`, canvas.width - 100, 30);
+}
+
+// draw bricks on canvas
+function drawBricks() {
+  bricks.forEach((column) => {
+    column.forEach((brick) => {
+      ctx.beginPath();
+      ctx.rect(brick.x, brick.y, brick.w, brick.h);
+      ctx.fillStyle = brick.visible ? "hsl(355, 91%, 55%)" : "transparent";
+      ctx.fill();
+      ctx.closePath();
+    });
+  });
+}
+
 // Draw everyting
 function draw() {
   drawBall();
   drawPaddle();
   drawScore();
-}
-
-// draw score on canvas
-function drawScore() {
-  ctx.font = "20px Arial";
-  ctx.fillText(`Score: ${score}`, canvas.width - 100, 30);
+  drawBricks();
 }
 
 draw();
